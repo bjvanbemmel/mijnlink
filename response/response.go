51d99ff8b856cc4a53766da45705c7c08d04e85a
bjvanbemmel <beauvanbemmel@live.nl>
2026-02-01 14:16:05 +0100

    fix content-size header

--- response/response.go
+++ response/response.go
@@ -28,7 +28,7 @@ func New(w http.ResponseWriter, value string, status int) {
 func NewFile(w http.ResponseWriter, value []byte, filename string) {
 	w.Header().Add("Content-Type", http.DetectContentType(value))
 	w.Header().Add("Content-Disposition", fmt.Sprintf("attachment; filename=%s", filename))
-	w.Header().Add("Content-Size", fmt.Sprintf("attachment; filename=%d", len(value)))
+	w.Header().Add("Content-Size", fmt.Sprintf("%d", len(value)))
 	w.Header().Add("Access-Control-Expose-Headers", "Content-Disposition,Content-Size")
 	w.WriteHeader(http.StatusOK)
 	w.Write(value)
@@ -36,7 +36,7 @@ func NewFile(w http.ResponseWriter, value []byte, filename string) {
 
 func NewFileHeaders(w http.ResponseWriter, value []byte, filename string) {
 	w.Header().Add("Content-Disposition", fmt.Sprintf("attachment; filename=%s", filename))
-	w.Header().Add("Content-Size", fmt.Sprintf("attachment; filename=%d", len(value)))
+	w.Header().Add("Content-Size", fmt.Sprintf("%d", len(value)))
 	w.Header().Add("Access-Control-Expose-Headers", "Content-Disposition,Content-Size")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte{})
